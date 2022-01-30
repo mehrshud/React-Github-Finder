c14ad5eba4df7e34aa8780ee7245aa71dc1aaf1e
Mehrshad Hamavandy <mehrshadhamavandy@gmail.com>
2022-01-30 13:52:53 +0330

    Initial Release

---
+++ src/components/layouts/Spinner.jsx
@@ -0,0 +1,16 @@
+import './assets/spinner.css';
+
+function Spinner() {
+    return (
+        <div className="w-100 mt-200 text-center mx-auto">
+            <div class="loadingio-spinner-ripple-jzlt8xl1ua">
+                <div class="ldio-w73erfq8kje">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;
